7ddbaac9902561d349df987fde999bbb2851d6a8
raikaat <raikaat@gmail.com>
2018-02-22 22:40:42 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
+  var zeros = 0; 
+  var denominator = 5;
+
+  while(denominator<number){
+  	zeros+=Math.floor(number/denominator);
+  	denominator*=5;
+  };
+  return zeros;
   // your implementation
 }
